da71803ea722b84e46faf9f0bb5dededf40db740
detko <detkovaannya@gmail.com>
2023-12-19 05:03:17 +0300

    Delete unused imports

--- backend/src/main/java/com/application/nosql2h23schedule/dto/ChainDto.java
+++ backend/src/main/java/com/application/nosql2h23schedule/dto/ChainDto.java
@@ -1,13 +1,9 @@
 package com.application.nosql2h23schedule.dto;
 
-import com.application.nosql2h23schedule.domain.Group;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.bson.types.ObjectId;
-
-import java.util.List;
 
 @Getter
 @Setter
