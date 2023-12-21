321e8d31c1c17c03887730cb0d4d96dfc297f04c
Natasha <rubezhovana@mail.ru>
2023-12-21 03:14:31 +0300

    Таблица с расписанием и парсом полученных items, накидывание стилейбазовых для таблицы

--- frontend/src/views/ScheduleView.vue
+++ frontend/src/views/ScheduleView.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="schedule-table">
-    schedule TABLE ffffffffff
+  <div class="main-background h-90 w-100">
+    <div class="white-background ma-11  pl-6 bg-white rounded-xl">
+      <div class="controls-row d-flex">
+        <div class="table-header font-weight-bold mt-8 ml-2 text-h4" style="letter-spacing: 1.28px;">Расписание</div>
+      </div>
+      <ScheduleTable/>
+    </div>
+
   </div>
 </template>
 
@@ -8,8 +14,12 @@
   setup
   lang="ts"
 >
+
+import ScheduleTable from '@/components/schedule/ScheduleTable.vue';
 </script>
 
 <style scoped>
-
+.main-background {
+  background-color: #F2F5F9;
+}
 </style>
